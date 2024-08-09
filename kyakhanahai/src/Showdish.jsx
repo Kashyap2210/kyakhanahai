6e016c2a547894c79bdb5bf0dd2d8e4475b18bca
Kashyap 2210 <kash.cdac@gmail.com>
2024-08-09 20:25:13 +0530

    Scroll Add To The Dish Table In 'See Added Dishes'

--- kyakhanahai/src/Showdish.jsx
+++ kyakhanahai/src/Showdish.jsx
@@ -62,39 +62,41 @@ export default function Showdish() {
   };
 
   return (
-    <div className="pt-16 gap-16 flex flex-col justify-around">
-      <div>
-        <h1 className="text-center mt-8 text-4xl font-bold	">Added Dishes</h1>
+    <div className="pt-8 gap-20 flex flex-col justify-around">
+      <div className="fixed top-20 left-0 w-full bg-white z-10">
+        <h1 className="text-center text-4xl font-bold">Added Dishes</h1>
       </div>
-      <div className=" flex justify-center items-center mb-16">
+      <div className=" flex justify-center items-center mt-16 mb-16 ">
         {/* Conditional rendering of the dish, if the dish is present then table is shown. */}
         {dishes.length > 0 ? (
           // Dishes data is shown in a tabular form
-          <table className="table-auto border border-black ">
-            <thead className="border border-black">
-              <tr className="border border-black">
-                <th className="hidden">ID</th>
-                <th className="border border-black p-4">Name</th>
-                <th className="border border-black p-4">Category</th>
-                <th className="border border-black p-4">Type</th>
-              </tr>
-            </thead>
-            <tbody>
-              {dishes.map((dish) => (
-                <tr key={dish.id}>
-                  <td className="hidden">{dish.id}</td>
-                  <td className="border border-black p-4">{dish.name}</td>
-                  <td className="border border-black p-4">{dish.category}</td>
-                  <td className="border border-black p-4">{dish.type}</td>
-                  <td className="border border-black p-4">
-                    <button onClick={() => deleteDish(dish.id)}>
-                      <DeleteOutlineIcon color="secondary"></DeleteOutlineIcon>
-                    </button>
-                  </td>
+          <div className="w-full max-w-4xl max-h-[400px] overflow-y-auto scrollbar scrollbar-none">
+            <table className="table-auto w-full border border-black">
+              <thead className="border border-black">
+                <tr className="border border-black">
+                  <th className="hidden">ID</th>
+                  <th className="border border-black p-4">Name</th>
+                  <th className="border border-black p-4">Category</th>
+                  <th className="border border-black p-4">Type</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {dishes.map((dish) => (
+                  <tr key={dish.id}>
+                    <td className="hidden">{dish.id}</td>
+                    <td className="border border-black p-4">{dish.name}</td>
+                    <td className="border border-black p-4">{dish.category}</td>
+                    <td className="border border-black p-4">{dish.type}</td>
+                    <td className="border border-black p-4">
+                      <button onClick={() => deleteDish(dish.id)}>
+                        <DeleteOutlineIcon color="secondary"></DeleteOutlineIcon>
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         ) : (
           <p>No dishes found.</p>
         )}
