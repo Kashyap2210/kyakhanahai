ce2d7948862674a6acf52ce4d83e6a595a00ce85
Kashyap 2210 <kash.cdac@gmail.com>
2024-08-18 01:45:58 +0530

    Header Add

--- kyakhanahai/src/Login.jsx
+++ kyakhanahai/src/Login.jsx
@@ -30,8 +30,8 @@ export default function Login() {
         {
           headers: {
             "Content-Type": "application/json",
+            withCredentials: true,
           },
-          withCredentials: true,
         }
       );
 
